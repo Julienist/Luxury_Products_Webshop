aa558678bdf41f69526bc719095a4861dfa18478
Julien Mijnders <s1142531@student.hsleiden.nl>
2025-06-01 02:48:46 +0200

    small import fix

--- frontend/src/app/services/admin.guard.ts
+++ frontend/src/app/services/admin.guard.ts
@@ -1,6 +1,6 @@
 import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
-import { UserService } from '../services/user.service';
+import { UserService } from './user.service';
 
 export const adminGuard: CanActivateFn = () => {
     const userService = inject(UserService);
