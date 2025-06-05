60bb55658ab4d105ce916add48680b2939a4c460
Julien Mijnders <s1142531@student.hsleiden.nl>
2025-06-05 15:28:59 +0200

    (Testing gedeelte): + InfirsfWebshopApiApplicationTests.java & + ValidationSuite.java
    hebben nu "ActiveProfiles", die in de application-test.properties werkt met h2 in-mem db, pg omzeilen bij testing.
    =================
    + docker-compose.yaml, mogelijkheid tot gebruik van pgadmin4 als interface. WEL uitgecomment.
    
    + testing with in-memory db now.
    no need to rely on postgresql for testing anymore.

--- backend/src/test/java/com/luxuryproductsholding/api/InfirsfWebshopApiApplicationTests.java
+++ backend/src/test/java/com/luxuryproductsholding/api/InfirsfWebshopApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.luxuryproductsholding.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class InfirsfWebshopApiApplicationTests {
 
     @Test
--- backend/src/test/java/com/luxuryproductsholding/api/ValidationSuite.java
+++ backend/src/test/java/com/luxuryproductsholding/api/ValidationSuite.java
@@ -3,9 +3,11 @@ package com.luxuryproductsholding.api;
 import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
+import org.springframework.test.context.ActiveProfiles;
 
 @Suite
 @SelectPackages("com.luxuryproductsholding.api.testclasses")
+@ActiveProfiles("test")
 @IncludeTags("validation")
 public class ValidationSuite {
 }
